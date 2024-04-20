45d928e5e81201c9a3bded7fe698e563dd362735
Adam Wiveion <wux024@nenu.edu.cn>
2024-04-20 15:24:15 +0800

    update

---
+++ tools/train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define datasets and model directories
+datasets=("fish")  # Add more dataset names here
+models=("yolov8n-pose.yaml" "yolov8s-pose.yaml" "yolov8m-pose.yaml" "yolov8l-pose.yaml" "yolov8x-pose.yaml" "yolov8x-pose-p6.yaml")
+
+# Train settings
+EPOCHS=100
+BATCH=-1
+IMGSZ=640
+DEVICE=None
+COS_LR=True
+RESUME=False
+
+# Traverse through the list of datasets and models
+for dataset in "${datasets[@]}"; do
+    for model in "${models[@]}"; do
+        # Derive the pre-trained model filename
+        pretrained="${model%.yaml}-pose.pt"
+        # Construct the training run name
+        name="${dataset}-${model%.yaml}"
+        project="./runs/pose/$dataset"
+
+        # Launch YOLO pose training command in background
+        yolo pose train \
+            data=./config/data/$dataset.yaml \
+            model=./config/models/$dataset/$model \
+            pretrained=./config/models/$dataset/$pretrained \
+            epochs=$EPOCHS \
+            imgsz=$IMGSZ \
+            batch=$BATCH \
+            project=$project \
+            name=$name \
+            device=$DEVICE \
+            cos_lr=$COS_LR \
+            resume=$RESUME
+    done
+done
