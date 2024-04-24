0a423e64faf5fced5fb933d23e1495769478a4a8
Adam Wiveion <wux024@nenu.edu.cn>
2024-04-24 20:44:57 +0800

    update

--- tools/train.sh
+++ tools/train.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-# Define datasets and model directories
-datasets=("aniposefly")  # Add more dataset names here
+# Define default train settings
+epochs=1000
+batch=-1
+imgsz=640
+device="None"
+cos_lr=true
+resume=false
+
+# Ensure a dataset name is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 dataset model1 [model2 ...]"
+    exit 1
+fi
+
+dataset="$1" # The first argument is the dataset name
+shift        # Shift arguments to left, so $@ now starts with model names
+
+# Models are predefined
 models=("yolov8n-pose.yaml" "yolov8s-pose.yaml" "yolov8m-pose.yaml" "yolov8l-pose.yaml" "yolov8x-pose.yaml" "yolov8x-pose-p6.yaml")
 
-# Train settings
-EPOCHS=100
-BATCH=-1
-IMGSZ=640
-DEVICE=None
-COS_LR=True
-RESUME=False
+# Loop through each model for the given dataset
+for model in "${models[@]}"; do
+    # Derive the pre-trained model filename
+    pretrained="${model%.yaml}.pt"
+    # Construct the training run name
+    name="${dataset}-${model%.yaml}"
+    project="./runs/pose/$dataset"
 
-# Traverse through the list of datasets and models
-for dataset in "${datasets[@]}"; do
-    for model in "${models[@]}"; do
-        # Derive the pre-trained model filename
-        pretrained="${model%.yaml}.pt"
-        # Construct the training run name
-        name="${dataset}-${model%.yaml}"
-        project="./runs/pose/$dataset"
-
-        # Launch YOLO pose training command in background
-        yolo pose train \
-            data=./configs/data/$dataset.yaml \
-            model=./configs/models/$dataset/$model \
-            pretrained=$pretrained \
-            epochs=$EPOCHS \
-            imgsz=$IMGSZ \
-            batch=$BATCH \
-            project=$project \
-            name=$name \
-            device=$DEVICE \
-            cos_lr=$COS_LR \
-            resume=$RESUME
-    done
+    # Launch YOLO pose training command
+    yolo pose train \
+        data=./configs/data/"$dataset".yaml \
+        model=./configs/models/"$dataset"/"$model" \
+        pretrained=$pretrained \
+        epochs=$epochs \
+        imgsz=$imgsz \
+        batch=$batch \
+        project=$project \
+        name=$name \
+        device=$device \
+        cos_lr=$cos_lr \
+        resume=$resume
 done
